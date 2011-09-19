337f41c03cfd75614d9bd8251eb60f758d5071ff
Spiderman.Alliance <Spiderman.Alliance@gmail.com>
2011-09-19 23:45:21 +0000

    + Added OSX Compatability Files & Docs + Updated Error Reports URL + Updated build number

--- src/org/alliance/core/CoreSubsystem.java
+++ src/org/alliance/core/CoreSubsystem.java
@@ -77,7 +77,7 @@ public class CoreSubsystem implements Subsystem {
     public final static int MB = 1024 * KB;
     public final static long GB = 1024 * MB;
     public final static int BLOCK_SIZE = MB;
-    public final static String ERROR_URL = "http://maciek.tv/alliance/errorreporter/";
+    public final static String ERROR_URL = "http://random.remyoukaour.com/alliance.php";
     private ResourceLoader rl;
     private FriendManager friendManager;
     private FileManager fileManager;
