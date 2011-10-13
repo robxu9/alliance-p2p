be3cf2adbc85d94b23cd1831531ad922a29573ca
remy.oukaour <remy.oukaour@gmail.com>
2011-10-13 03:21:09 +0000

    + Added a failsafe maximum chat history length of 2000.

--- src/org/alliance/core/settings/Internal.java
+++ src/org/alliance/core/settings/Internal.java
@@ -574,7 +574,7 @@ public class Internal extends SettingClass {
     }
     
     public void setChatmaxlines(Integer chatmaxlines) {
-    	if (chatmaxlines >= 0) {
+    	if (chatmaxlines >= 0 && chatmaxlines <= 2000) { // failsafe maximum
     		this.chatmaxlines = chatmaxlines;
     	}
     }
@@ -584,7 +584,7 @@ public class Internal extends SettingClass {
     }
     
     public void setChathistorymaxlines(Integer chathistorymaxlines) {
-      if (chathistorymaxlines >= 0) {
+      if (chathistorymaxlines >= 0 && chathistorymaxlines <= 2000) { // failsave maximum
     		this.chathistorymaxlines = chathistorymaxlines;
     	}
     }
