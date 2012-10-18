fb820f53a0a6b82991935c9d10c751b5ddb43934
remy.oukaour@gmail.com <remy.oukaour@gmail.com@7f8a7933-acf7-43eb-9f76-72b24cfb2564>
2012-10-18 04:19:40 +0000

    + Updated build + Updated language file + Localized TV Bot strings

--- trunk/src/org/alliance/misc/ChatBots.java
+++ trunk/src/org/alliance/misc/ChatBots.java
@@ -7,6 +7,8 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLEncoder;
 
+import org.alliance.core.Language;
+
 /**
  * TV Bot has been implemented with the TVRage API:
  * http://services.tvrage.com/tools/quickinfo.php?show=QUERY
@@ -45,8 +47,7 @@ public class ChatBots {
 		String line;
 		while (!(((line = in.readLine()) == null))) {
 			if (line.startsWith("No Show")) {
-				// TODO: localize this string
-				return botValue("No such show!");
+				return botMessage(botValue(Language.getLocalizedString(ChatBots.class, "noshow", show)));
 			}
 			else if (line.startsWith("Show Name@")) {
 				showName = botValue(line.substring(line.indexOf("Show Name@") + "Show Name@".length()));
@@ -81,10 +82,10 @@ public class ChatBots {
 		StringBuilder sb = new StringBuilder();
 		sb.append("[<a href=\"" + linkURL + "\">" + showName + "</a>]");
 		if (!ended.isEmpty()) {
-			sb.append(" :: [Ended: " + botValue(ended) + "]");
+			sb.append(" :: [Ended on " + botValue(ended) + "]");
 		}
 		else {
-			sb.append(" :: [Airs: " + airTime + " on " + network + "]");
+			sb.append(" :: [Airs at " + airTime + " on " + network + "]");
 			sb.append(" :: [" + botValue(nextEpisodeDate) + " " + botValue(daysUntilAirs) + "d" + botValue(hoursUntilAirs) + "h" +
 					botValue(minutesUntilAirs) + "m from now]");
 			sb.append(" :: [" + nextEpisode + "]");
